b9b5e48a79aff26aa946e4bb6c0b407522263230
Ayush Kumar <ayushkumar200264@gmail.com>
2025-01-14 14:56:56 +0530

    fixed font sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className=" absolute px-2 md:px-8 py-2 w-full z-10">
+    <div className=" absolute px-2 md:px-8 py-1 w-full z-10">
       <img 
-          className="w-32 md:w-44 lg:w-64 mx-1 lg:mx-44 md:mx-16 sm:mx-1 my-0.5 saturate-150 "
+          className="w-24 md:w-32 lg:w-48 mx-1 lg:mx-32 md:mx-16 sm:mx-1 my-0.5 saturate-150 "
           src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
           alt="logo"
         />
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -16,21 +16,37 @@ const LoginPage = () => {
           className="w-screen min-h-screen scale-125 brightness-0 md:brightness-50"
         />
       </div>
-      <form className="w-full md:w-[600px] absolute p-6 md:p-24 bg-black my-36 mx-auto right-0 left-0  text-white rounded-lg bg-opacity-70 ">
-        <h1 className="font-bold text-5xl mb-8">
+      <form className="w-full md:w-[450px] absolute p-6 md:px-[70px] md:py-12 bg-black my-24 mx-auto right-0 left-0  text-white rounded-lg bg-opacity-70 ">
+        <h1 className="font-bold text-[33px] mb-8">
           {signInForm ? "Sign In" : "Sign Up"}
         </h1>
+        {!signInForm && <div className="relative">
+          <input
+            tyoe="text"
+            id="fullName"
+            className="block p-4 my-4 w-full bg-black bg-opacity-5 rounded-md inner-border-2 border-white peer"
+            placeholder=""
+            autoComplete="name"
+          />
+          <label
+            htmlFor="fullName"
+            className="absolute ml-2 text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/3 rtl:peer-focus:left-auto"
+          >
+            Enter your full name
+          </label>
+          
+        </div>}
         <div className="relative">
           <input
             type="text"
             id="email"
-            className="block p-6 my-4 w-full bg-black bg-opacity-5 rounded-md inner-border-2 border-white peer"
+            className="block p-4 my-4 w-full bg-black bg-opacity-5 rounded-md inner-border-2 border-white peer"
             placeholder=" "
             autoComplete="email"
           />
           <label
             htmlFor="email"
-            className="absolute ml-3 text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-5 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+            className="absolute ml-2 text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/3 rtl:peer-focus:left-auto"
           >
             Email or mobile number
           </label>
@@ -39,28 +55,28 @@ const LoginPage = () => {
           <input
             type="password"
             id="password"
-            className="block p-6 my-4 w-full bg-black bg-opacity-5 rounded-md inner-border-2 border-white peer"
+            className="block p-4 my-4 w-full bg-black bg-opacity-5 rounded-md inner-border-2 border-white peer"
             placeholder=" "
             autoComplete="password"
           />
           <label
             htmlFor="password"
-            className="absolute ml-3 text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-5 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+            className="absolute ml-2 text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/3 rtl:peer-focus:left-auto"
           >
             Password
           </label>
         </div>
         <label className="inline-flex items-center cursor-pointer">
-          <input type="checkbox"  className=" size-6"/>
-          <span className="ml-3 text-xl ">Remember me</span>
+          <input type="checkbox"  className=" size-4"/>
+          <span className="ml-3 text-base ">Remember me</span>
         </label>
         <button
           type="submit"
-          className="p-6 my-8 w-full font-medium text-xl bg-red-600 rounded-lg"
+          className="p-3 my-8 w-full font-medium text-base bg-red-600 rounded-lg"
         >
           {signInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className="py-4 text-xl">
+        <p className="py-4 text-base">
           <span className=" text-gray-300">
             {signInForm ? "New to NetFlix? " : "Already a member? "}
           </span>
